3efea8d8cbb1bf5a87e30d2cf741759ce9217998
noogen <tom@noogen.net>
2018-11-21 11:55:13 -0600

    update test

--- run-data-tests.sh
+++ run-data-tests.sh
@@ -2,10 +2,10 @@
 export DEBUG=lambda-form
 export SLS_DEBUG=*
 
-#serverless invoke local -f formPostHandler -p tests/404.json
-#serverless invoke local -f formPostHandler -p tests/deleted.json | grep 'Form deleted no longer accept submission.'
-#serverless invoke local -f formPostHandler -p tests/email-custom.json | grep '"https://www.example.com/thank-you-page'
+serverless invoke local -f formPostHandler -p tests/404.json
+serverless invoke local -f formPostHandler -p tests/deleted.json | grep 'Form deleted no longer accept submission.'
+serverless invoke local -f formPostHandler -p tests/email-custom.json | grep '"https://www.example.com/thank-you-page'
 serverless invoke local -f formPostHandler -p tests/email-default.json | grep 'email-default submission accepted.'
-#serverless invoke local -f formPostHandler -p tests/honeypot.json | grep 'Missing data in submission.'
-#serverless invoke local -f formPostHandler -p tests/origins.json | grep 'Invalid origin (http://localhost:3000) submission.'
-#serverless invoke local -f formPostHandler -p tests/recaptcha.json | grep 'Invalid captcha (undefined) response.'
+serverless invoke local -f formPostHandler -p tests/honeypot.json | grep 'Missing data in submission.'
+serverless invoke local -f formPostHandler -p tests/origins.json | grep 'Invalid origin (http://localhost:3000) submission.'
+serverless invoke local -f formPostHandler -p tests/recaptcha.json | grep 'Invalid captcha (undefined) response.'
