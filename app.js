f8400d99b3754c3df77c78491f228e16183c54aa
noogen <tom@noogen.net>
2018-11-25 13:51:03 -0600

    update path parsing

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ require("@babel/register")
 
 const post = require('./lib/postHandler')
 const http = require('http')
+const URL  = require('url')
 
 http.createServer((req, res) => {
+  const url   = URL.parse(req.url, true)
   const ctype = req.headers['Content-Type'] | req.headers['content-type']
+
+  // parse query string
+  req.queryStringParameters = url.query
+
+  // parse form id
+  const path = url.pathname.replace(/^\/*|\/*$/g, '')
+  req.pathParameters = {
+    id: path.split('/')[0]
+  }
+
   if (ctype.indexof('/json') > -1) {
     let data = ''
     req.on('data', (chunk) => { data += chunk })
