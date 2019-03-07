c664e211e9075a3e5c73160eaacb294e1d507961
noogen <tom@noogen.net>
2019-03-07 10:59:23 -0600

    update schema

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 import submission from './lib/submissionHandler'
 import post from './lib/postHandler'
+import auth from './lib/authHandler'
 
 export const formPostHandler = post
 export const submissionHandler = submission
+export const authHandler = auth
