2b68c0c1bbb5b9f2c61d892669d491ed47c4b6db
noogen <tom@noogen.net>
2020-12-29 16:00:02 -0600

    update demo

---
+++ demo/index.js
@@ -0,0 +1,63 @@
+var $form = $('#contact-form');
+// init the validator
+// validator files are included in the download package
+// otherwise download from http://1000hz.github.io/bootstrap-validator
+$form.validator();
+
+function onClick(e) {
+  debugger;
+  var input = $('#g-recaptcha-response');
+
+  grecaptcha.ready(function() {
+    grecaptcha.execute(input.data('sitekey'), {action: input.data('action')}).then(function(rsp) {
+      // Add your logic to submit to your backend server here.
+      // https://developers.google.com/recaptcha/docs/verify
+      $form.submit();
+    });
+  });
+};
+
+$(function () {
+  // when the form is submitted
+  $form.on('submit', function (e) {
+
+    // if the validator does not prevent form submit
+    if (!e.isDefaultPrevented()) {
+
+      // use regular form submit then only validate, don't ajax submit
+      if ($('#form_use').val() === '1') {
+        return true;
+      }
+
+      var url = $form.attr('action');
+
+      // POST values in the background the the script URL
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: $form.serialize(),
+        success: function (data)
+        {
+          // data = JSON object that returns
+
+          // we recieve the type of the message: success x danger and apply it to the
+          var messageAlert = 'alert-' + data.type;
+          var messageText = data.message;
+
+          // let's compose Bootstrap alert box HTML
+          var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+
+          // If we have messageAlert and messageText
+          if (messageAlert && messageText) {
+            // inject the alert to .messages div in our form
+            $form.find('.messages').html(alertBox);
+            // empty the form
+            $form[0].reset();
+          }
+        }
+      });
+
+      return false;
+    }
+  });
+});
